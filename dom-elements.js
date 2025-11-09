33c4976b3c39461ef39446532219a80593240db5
Kez 92 <kez92@MacBook-Air-Kez.local>
2025-11-10 01:51:26 +0300

    добавил обработчик на имя и комментарий - нулевос лайк и форму в конец списка

--- dom-elements.js
+++ dom-elements.js
@@ -1,8 +1,9 @@
 
 
-const inputNameElement = document.getElementById('inputName');
-const commentsTextElement = document.getElementById('commentsText');
-
+export const inputNameElement = document.getElementById('inputName');
+export const commentsTextElement = document.getElementById('commentsText');
+export const containerCommentsElement = document.getElementById('comment')
+export const writeButtonCommentEl = document.getElementById('writeButtonComment');
 
 
 
