68538214297b9be62334bec5e2bf2ec7bb7ea3dc
Kez 92 <kez92@MacBook-Air-Kez.local>
2025-11-10 20:17:06 +0300

    add render coments html

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
 import {inputNameElement, commentsTextElement ,containerCommentsElement, writeButtonCommentEl} from './dom-elements.js';
 
+
+const comments = [
+    {
+        name: "Глеб Фокин",
+        date: "12.02.22 12:18",
+        text: "Это будет первый комментарий на этой странице",
+        likes: 3,
+        like: false
+    },
+    {
+        name: "Варвара Н.",
+        date: "13.02.22 19:22",
+        text: "Мне нравится как оформлена эта страница! ❤",
+        likes: 75,
+        isLiked: true
+    }
+];
+
+function renderComments(){
+    const commentsHtml = comments.map(comment =>{
+        let likeClass;
+        if (comment.isLiked === true) {
+            likeClass = ' -active-like';
+        } else {
+            likeClass = '';
+        }
+        return `
+             <li class="comment">
+              <div class="comment-header">
+                <div>${comment.name}</div>
+                <div>${comment.date} </div>
+              </div>  <!-- ← Закрывающий div -->
+              <div class="comment-body">
+                <div class="comment-text">
+                  ${comment.text}
+                </div>
+              </div>
+              <div class="comment-footer">
+                <div class="likes">
+                  <span class="likes-counter">${comment.likes}</span>
+                  <button class="like-button${likeClass}"></button> 
+                </div>
+              </div>
+            </li>
+        `
+    }).join("");
+    containerCommentsElement.innerHTML = commentsHtml;
+}
+renderComments();
 inputNameElement.addEventListener('input',function (ev){
     if (inputNameElement.value === ""){
         alert('Введите имя!')
@@ -48,3 +97,4 @@ writeButtonCommentEl.addEventListener('click', function (ev){
     inputNameElement.value = "";
     commentsTextElement.value = "";
 });
+
